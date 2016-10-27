b92666ea42aa9a8e123bdb0042fd2a22134625a9
DmitryO <ordyntcev.dmitry@e-queo.com>
2016-10-27 16:59:07 +0300

    fix

---
+++ src/com/company/Main.java
@@ -0,0 +1,31 @@
+package com.company;
+
+import java.util.Scanner;
+
+class Main {
+
+    public static void main(String[] args) {
+
+
+        Scanner scan = new Scanner(System.in);
+        String A = scan.next();
+        String B = scan.next();
+        int sum = A.length()+B.length();
+        System.out.println(sum);
+        if(A.length()<B.length())
+        {
+            System.out.println("Yes");
+        }
+        else   {System.out.println("No");}
+
+        String c = A.substring(0,1).toUpperCase()+A.substring(1);
+        String d = B.substring(0,1).toUpperCase()+B.substring(1);
+        if(A.length()>B.length()) {
+            System.out.println(c + " " + d);
+        }
+        else {
+            System.out.println(d + " " + c);
+        }
+
+    }
+}
