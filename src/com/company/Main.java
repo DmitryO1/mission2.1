b921491b64f39dd824dbe83ffeb425dee6094fb4
DmitryO <ordyntcev.dmitry@e-queo.com>
2016-10-31 18:06:52 +0300

    New fix

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,13 +6,12 @@ class Main {
 
     public static void main(String[] args) {
 
-
         Scanner scan = new Scanner(System.in);
         String A = scan.next();
         String B = scan.next();
         int sum = A.length()+B.length();
         System.out.println(sum);
-        if(A.length()<B.length())
+        if(A.compareTo(B) >0)
         {
             System.out.println("Yes");
         }
@@ -20,12 +19,10 @@ class Main {
 
         String c = A.substring(0,1).toUpperCase()+A.substring(1);
         String d = B.substring(0,1).toUpperCase()+B.substring(1);
-        if(A.length()>B.length()) {
-            System.out.println(c + " " + d);
-        }
-        else {
-            System.out.println(d + " " + c);
-        }
+        System.out.println(c+" "+d);
+
+
+
 
     }
 }
